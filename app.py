8d863f555025b06777a1d6efe5f686ef5efa2b98
Burak Tilek <burak.tilek@gmail.com>
2020-04-06 22:35:44 +0300

    /artists/search and /venues/search endpoints are implemented

--- app.py
+++ app.py
@@ -150,18 +150,18 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for Hop should return "The Musical Hop".
-    # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+
+    search_term = request.form.get('search_term', '')
+
+    result = Venue.query.with_entities(Venue.id.label(
+        'id'), Venue.name.label('name'), func.count().label('num_upcoming_shows')).join(Show, (Show.venue_id == Venue.id) & (Show.start_time > datetime.now())).filter(Venue.name.ilike('%'+search_term+'%')).group_by(Venue.id).all()
+
     response = {
-        "count": 1,
-        "data": [{
-            "id": 2,
-            "name": "The Dueling Pianos Bar",
-            "num_upcoming_shows": 0,
-        }]
+        "count": len(result),
+        "data": result
     }
-    return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
+
+    return render_template('pages/search_venues.html', results=response, search_term=search_term)
 
 
 @app.route('/venues/<int:venue_id>')
@@ -259,18 +259,18 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
+
+    search_term = request.form.get('search_term', '')
+
+    result = Artist.query.with_entities(Artist.id.label(
+        'id'), Artist.name.label('name'), func.count().label('num_upcoming_shows')).join(Show, (Show.artist_id == Artist.id) & (Show.start_time > datetime.now())).filter(Artist.name.ilike('%'+search_term+'%')).group_by(Artist.id).all()
+
     response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+        "count": len(result),
+        "data": result,
     }
-    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+
+    return render_template('pages/search_artists.html', results=response, search_term=search_term)
 
 
 @app.route('/artists/<int:artist_id>')
